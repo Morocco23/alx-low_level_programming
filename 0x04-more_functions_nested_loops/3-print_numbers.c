01c2a4af8b8ff592e80f1cd14fce502653b620a4
Daniel Eniola Favour <eniolad206@gmail.com>
2023-12-17 23:02:54 +0000

    prit numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main-
+ * function prints 0-9
+ * using only two putchar function
+ */
+void print_numbers(void)
+{
+	for(int i=0;i < 10;i++)
+	{
+		putchar('0' + i)
+			putchar('\n')
+	}
+}
