0a6545b994e7aa9f0da83fe06f31185d6cee7837
Daniel Eniola Favour <eniolad206@gmail.com>
2023-12-17 22:35:20 +0000

    correction

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  *mul- multiplies two integers
- *@a:recieved an integer
- *@b: recieves an integer
- *Return: multi value
+ *@x: The first number to mutiply
+ *@y: The second number to multiply with the first number
+ *Return: The result of multiplication between x and y param
  */
-int mul(int a, int b)
+int mul(int x, int y)
 {
-   return (a * b);
+   return (x * y);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 int _isupper(int c);
 int _putchar(char c);
 int _isdigit(int c);
-int mul(int a, int b);
+int mul(int x, int y);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
